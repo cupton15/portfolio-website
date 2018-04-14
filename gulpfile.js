9aaec4ad23287a5c8e290e3e2285f34fe30a9242
Carl Upton <carlupton15@gmail.com>
2018-04-14 19:04:11 +0100

    adding sass auto compile

--- gulpfile.js
+++ gulpfile.js
@@ -3,13 +3,11 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 
 gulp.task('sass', function() {
-    gulp.src('*.scss')
+    gulp.src('./css/*.scss')
         .pipe(sass())
-        .pipe(gulp.dest(function(f) {
-            return f.base;
-        }))
+        .pipe(gulp.dest('./css/'))
 });
 
-gulp.task('default', ['sass'], function() {
-    gulp.watch('*.scss', ['sass']);
+gulp.task('watch-sass', ['sass'], function() {
+    gulp.watch('./css/*.scss', ['sass']);
 })
