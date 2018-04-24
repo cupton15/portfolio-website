4efee0dd46e807cb94b81b99f17b15492c65e80d
Carl Upton <carlupton15@gmail.com>
2018-04-24 21:06:50 +0100

    small css changes

--- js/main.js
+++ js/main.js
@@ -1,4 +1,14 @@
 $(document).ready(function() {
+    $('.nav-link').on('click', function(){
+        $('.content').css('display', 'none');
+        $('.content').fadeIn(500);
+
+        if($('#nav-toggle').is(':visible')){
+            $('#nav-toggle').toggleClass('active');
+            $('nav .nav-container').hide('slide', { direction: 'right'}, 500);   
+        }
+    })
+
     $('#nav-toggle').on('click', function() {
         this.classList.toggle('active');
         $('nav .nav-container').toggle('slide', {direction: 'right'}, 500);
