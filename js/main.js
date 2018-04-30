dead30c834e94d7684aa4ea147f213b2dd1266b6
Carl Upton <carlupton15@gmail.com>
2018-04-30 22:22:19 +0100

    css styling change

--- js/main.js
+++ js/main.js
@@ -11,6 +11,8 @@ $(document).ready(function() {
         }
     })
 
+
+
     $('#nav-toggle').on('click', function() {
         this.classList.toggle('active');
         $('nav .nav-container').toggle('slide', {direction: 'right'}, 500);
